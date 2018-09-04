abababca693a7db5336e4155b640d0755fe03b17
James Blair <james.m.blair09@gmail.com>
2018-09-04 13:45:26 -0600

    Initial hw1 commit

---
+++ files/cheese.R
@@ -0,0 +1,18 @@
+# Read into R the data in the files "cheese1.csv" and "cheese2.csv".  These
+# datasets contain the per capita consumption of different cheeses from 1995 to
+# 2014. The two data sets cover the same years, although the names for the
+# variable holding the year differ.  Merge the data together into a single
+# data.frame in which all the consumption data for a given year is one
+# observation.  Name your merged data.frame "cheese".  The dimension of this
+# data.frame should be 20 rows and 7 columns.
+
+
+
+# Which kind of cheese has the highest average consumption per capita over all
+# years?
+
+
+
+# Which year saw the largest total consumption per capita of all cheeses?
+
+
---
+++ files/clothes.R
@@ -0,0 +1,22 @@
+# Read in the data from "clothes.csv" file into a data.frame called "clothes".
+# Remove all rows from the data.frame where the variable "Price" is empty,
+# i.e., equal to "". 
+
+
+# Write a function called "num" that takes a character vector of prices as
+# input, removes the dollar sign, and returns a numeric vector of prices.
+
+num <- function(string) { 
+  # Your code goes here.
+} 
+
+
+
+# Using the "num" function you just wrote, convert the variable "Price" in the
+# clothes data.frame from a character vector to a numeric vector.
+
+
+# Determine the standard deviation of the price of clothes for each part of the
+# body, as indicated by the variable "Body.Location".
+
+
